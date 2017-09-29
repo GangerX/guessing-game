672054b12337c68c39ab5b648e300555b191de34
GangerX <job_maslov@bk.ru>
2017-09-29 17:13:33 +0300

    Game-easy

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,16 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {var min_temp, max_temp, middle_temp;}
 
     setRange(min, max) {
-
+        this.min_temp = min;
+        this.max_temp = max;
     }
 
-    guess() {
+    guess() {return (this.middle_temp = Math.round((this.min_temp + this.max_temp) / 2));}
 
-    }
+    lower() {return (this.max_temp = this.middle_temp);}
 
-    lower() {
+    greater() {return (this.min_temp = this.middle_temp);}
 
-    }
-
-    greater() {
-
-    }
 }
-
 module.exports = GuessingGame;
